547dd13c9d67dc3dc638328949afb7776b7e7c2f
kyotostada <jereisastar@gmail.com>
2026-08-31 16:52:44 -0300

    Update EX2.pas

--- Resoluciones/P3/EX2.pas
+++ Resoluciones/P3/EX2.pas
@@ -17,13 +17,13 @@ type
 
 // ÁRBOL 1
   venta = record
-    codigo: inte
+    codigo: integer;
     fec: fecha;
     cantVentas: integer; 
   end;
 
 
-  arbol1 = ^nodoVenta
+  arbol1 = ^nodoVenta;
 
   nodoVenta = record
     elem: venta;
@@ -37,7 +37,7 @@ type
     totalVentas: integer;
   end;
 
-  arbolProducto = ^nodoProducto
+  arbolProducto = ^nodoProducto;
 
   nodoProducto = record
     elem: producto;
@@ -51,7 +51,7 @@ type
     cant: integer;
   end;
 
-  lista = ^nodoLista
+  lista = ^nodoLista;
 
   nodoLista = record
     elem: vental;
@@ -65,7 +65,7 @@ type
     lis: lista;
   end;
 
-  arbolProductoVentas = ^nodoProductoVentas
+  arbolProductoVentas = ^nodoProductoVentas;
 
   nodoProductoVentas = record
     elem: venta2;
@@ -73,113 +73,109 @@ type
     hd: arbolProductoVentas;
   end;
 
-procedure generarDatos (var V1: venta; var P: productoM; var v2: venta2; var vl: vental);
+procedure generarDatos (var V1: venta; var P: producto; var v2: venta2; var vl: vental);
 begin
   // Venta Árbol 1
-  v1.code:= random(101);
-  if (v.code <> fin) then
+  v1.codigo:= random(101);
+  if (v1.codigo <> fin) then
     v1.fec.dia:= random (30) + 1;
     v1.fec.mes:= random(12) + 1;
     v1.fec.anio:= random (2026-2000) + 2000;
-    v1.cant:= random(1001);
+    v1.cantVentas:= random(1001)+1;
   // Producto Árbol 2
-  p.code:= v1.code
-  p.totalVentas:= v1.cant;
+  p.code:= v1.codigo;
+  p.totalVentas:= v1.cantVentas;
   // Venta Árbol 3
-  v2.code:= v1.code
+  v2.code:= v1.codigo;
   // Venta lista
   vl.fec:= v1.fec;
-  vl.cant:= v1.cant;
+  vl.cant:= v1.cantVentas;
 end;
 
 
-procedure generarArboles (var AV: arbol1; var AP: arbolProducto; var APV: arbolProductoVentas; var V1: venta; var P: productoM; var v2: venta2);
+procedure generarArboles (var AV: arbol1; var AP: arbolProducto; var APV: arbolProductoVentas; var V1: venta; var P: producto; var v2: venta2);
 
   Procedure cargarArbol1 (var a: arbol1; v: venta);
   Begin
     if (a = nil) 
     then begin
            new(a);
-           a^.dato:= v; 
-           a^.HI:= nil; 
-           a^.HD:= nil;
+           av^.elem:= v; 
+           av^.HI:= nil; 
+           av^.HD:= nil;
          end
-    else if (elem.numero < a^.elem.code) 
-         then cargarArbol1(a^.HI, elem)
-         else cargarArbol1(a^.HD, elem); 
+    else if (v.codigo < av^.elem.codigo) 
+         then cargarArbol1(av^.HI, v)
+         else cargarArbol1(av^.HD, v); 
   End;
   
   Procedure cargarArbol2 (var a: arbolProducto; p: producto);
   Begin
     if (a = nil) then begin
       new(a);
-      a^.dato:= p; 
+      a^.elem:= p; 
       a^.HI:= nil; 
       a^.HD:= nil;
-    end;
+    end
     else
-      if (a.code = p.code) then
-        a^.dato.cant := a^.dato.cant + p.cant
-    end;
+      if (a^.elem.code = p.code) then
+        a^.elem.totalVentas := a^.elem.totalVentas + p.totalVentas
       else
-        if (elem.numero < a^.elem.code) 
+        if (p.code < a^.elem.code) 
           then cargarArbol2(a^.HI, p)
           else cargarArbol2(a^.HD, p); 
       end;
-  End;
 
   Procedure cargarLista (var l: lista; vl: vental);
   var
     nue: lista;
   begin
     new(nue);
-    nue^.elem:= v;
+    nue^.elem:= vl;
     nue^.sig:= l;
     l:= nue;
   end;
 
   Procedure cargarArbol3 (var a: arbolProductoVentas; v: venta2; vl: vental);
   begin
-    if (a = nil) then
+    if (a = nil) then begin
       new(a);
-      a^.elem.lista := nil;
-      cargarLista(a^.elem.lista, vl);
+      a^.elem.lis := nil;
+      cargarLista(a^.elem.lis, vl);
       a^.elem.code:= v.code;
       a^.hi:= nil;
       a^.hd:= nil;
-    end;
-    else begin
+    end
+    else
       if (v.code = a^.elem.code) then
-        cargarLista(a^.elem.lista, vl);
-      else begin
+        cargarLista(a^.elem.lis, vl)
+      else
         if (v.code < a^.elem.code) then
-          cargarArbol3((a^.HI, vl));
+          cargarArbol3(a^.HI, v, vl)
         else
-          if (v.code > a^.elem.code) then
-            cargarArbol3((a^.HD, vl));
+            cargarArbol3(a^.HD, v, vl);
       end;
-  end;
 
 var
   vl: vental;
 begin
-  a:= nil;
   av:= nil;
+  ap:= nil;
   apv:= nil;
-  lectura(v1, p, v2, vl);
-  while (v.cod <> fin) do begin
-    cargarArbol1(av, v1)
+  generarDatos(v1, p, v2, vl);
+  while (v1.codigo <> fin) do begin
+    cargarArbol1(av, v1);
     cargarArbol2(ap, p);
-    cargarArbol3(apv, v2);
+    cargarArbol3(apv, v2, vl);
     generarDatos(v1,p,v2,vl);
   end;
 end;
 
-procedure CantidadFecha (a: arbol1; f: fecha; var cant);
+procedure CantidadFecha (a: arbol1; f: fecha; var cant: integer);
 begin
   if (a <> nil) then begin
-    if (a^.elem.fec.dia = f.dia) and (a^.elem.fec.mes = f.mes) and (a^.elem.fec.año = f.anio) then
-      cant:= cant + a^.elem.cant;
+    if (a^.elem.fec.dia = f.dia) and (a^.elem.fec.mes = f.mes) and (a^.elem.fec.anio = f.anio) then
+      cant:= cant + a^.elem.cantVentas
     else begin
       CantidadFecha(a^.hi, f,cant);
       CantidadFecha(a^.hd, f,cant);
@@ -189,13 +185,13 @@ end;
 
 procedure MaxUnidades (AV: arbolProducto; var max, maxcode: integer);
 begin
-  if (av <> nil) do begin
-    if (av^.elem.cant > max) then begin
-      max:= av^.elem.cant
+  if (av <> nil) then begin
+    if (av^.elem.totalVentas > max) then begin
+      max:= av^.elem.totalVentas;
       maxcode:= av^.elem.code
-    end;
+    end
     else begin
-      MaxUnidades(av^.hi, max, maxcode)
+      MaxUnidades(av^.hi, max, maxcode);
       MaxUnidades(av^.hd, max, maxcode);
     end;
   end;
@@ -217,29 +213,45 @@ var
   aux: integer;
 begin
   if (apv <> nil) then
-    recorrerlista (apv^.elem.lista, aux);
+    recorrerlista (apv^.elem.lis, aux);
     if aux > max then begin
-      max:= aux
-      maxcode:= apv^.elem.code
+      max:= aux;
+      maxcode:= apv^.elem.code;
     end;
     MaxVentas (apv^.hi, max,maxcode);
     MaxVentas  (apv^.hd, max,maxcode);
+end;
+
+
+procedure leerFecha(var f: fecha);
+begin
+  read(f.dia);
+  read(f.mes);
+  read(f.anio);
+end;
 var
   arbolV: arbol1;
   arbolP: arbolProducto;
   arbolPV: arbolProductoVentas;
+  v1: venta;
+  v2: venta2;
+  p: producto;
   f: fecha;
   CantFecha: integer;
   maxcode, maxven: integer;
+  maxcant: integer;
+  maxcantVen: integer;
 begin
+  maxcant:= -1;
+  maxcantVen:= -1;
   randomize;
-  generarArboles (arbolV, arbolP, arbolPV);
+  generarArboles (arbolV, arbolP, arbolPV, v1, p, v2);
   writeln('Ingrese una fecha:');
   leerFecha(f);
-  CantidadFecha(arbolV, f, cant)
-  writeln('La cantidad de ventas en la fecha:', f, 'es de:', cantfecha);
-  MaxUnidades(arbolP, max, maxcode);
-  writeln('El código de producto con mayor cantidad de unidades vendidas es:', maxuni);
-  MaxVentas(arbolPV, maxven);
+  CantidadFecha(arbolV, f, cantFecha);
+  writeln('La cantidad de ventas en la fecha es de:', cantfecha);
+  MaxUnidades(arbolP, maxcant, maxcode);
+  writeln('El código de producto con mayor cantidad de unidades vendidas es:', maxcant);
+  MaxVentas(arbolPV, maxcantVen, maxven);
   writeln('El código de producto con mayor cantidad de ventas es:', maxven)
-end;
+end.
